13fe47ad71e0c5a99f92b1b63435bba67808237c
dempseyevans <dbray2@my.gcu.edu>
2024-08-03 22:35:23 -0400

    yml

--- app/src/main/java/com/cst323/app/service/userService.java
+++ app/src/main/java/com/cst323/app/service/userService.java
@@ -3,6 +3,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,7 @@ public class userService {
     @Autowired
     private userRepository userRepository;
 
-    private static final Logger logger = Logger.Factory(userService.class);
+    private static final Logger logger = LoggerFactory.getLogger(userService.class);
 
     public List<user> getAllUsers()
     {
